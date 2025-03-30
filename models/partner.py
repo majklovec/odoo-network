4e5fa8b17c3aecbe74e241bfa93dfe998cf79396
Michal Vondracek <info@michalvondracek.cz>
2025-03-30 14:53:37 +0200

    fix(partner): do not add user

--- models/partner.py
+++ models/partner.py
@@ -15,74 +15,74 @@ class ResPartner(models.Model):
     user_id = fields.Many2one("res.users", string="Linked User")
     login = fields.Char(string="Login", related="user_id.login", readonly=False)
 
-    @api.model
-    def create(self, vals_list):
-        # Extract login and password from each vals and remove them
-        extracted_data = []
-        for vals in vals_list:
-            login = vals.pop("login", False)
-            password = vals.pop("password", False)
-            extracted_data.append({"login": login, "password": password})
+    # @api.model
+    # def create(self, vals_list):
+    #     # Extract login and password from each vals and remove them
+    #     extracted_data = []
+    #     for vals in vals_list:
+    #         login = vals.pop("login", False)
+    #         password = vals.pop("password", False)
+    #         extracted_data.append({"login": login, "password": password})
 
-        # Create partners in batch
-        partners = super().create(vals_list)
+    #     # Create partners in batch
+    #     partners = super().create(vals_list)
 
-        # Create users for each partner with extracted data
-        for partner, data in zip(partners, extracted_data):
-            login = data["login"]
-            password = data["password"]
-            if login:
-                existing_user = self.env["res.users"].search(
-                    [("login", "=", login)], limit=1
-                )
-                if existing_user:
-                    raise exceptions.ValidationError(
-                        _("Login %s already exists in the system") % login
-                    )
-                if not password:
-                    raise exceptions.ValidationError(
-                        _("Password is required when creating a user")
-                    )
-                user_vals = {
-                    "login": login,
-                    "password": password,
-                    "partner_id": partner.id,
-                    "groups_id": [(6, 0, [])],
-                }
-                user = self.env["res.users"].create(user_vals)
-                partner.user_id = user.id
+    #     # Create users for each partner with extracted data
+    #     for partner, data in zip(partners, extracted_data):
+    #         login = data["login"]
+    #         password = data["password"]
+    #         if login:
+    #             existing_user = self.env["res.users"].search(
+    #                 [("login", "=", login)], limit=1
+    #             )
+    #             if existing_user:
+    #                 raise exceptions.ValidationError(
+    #                     _("Login %s already exists in the system") % login
+    #                 )
+    #             if not password:
+    #                 raise exceptions.ValidationError(
+    #                     _("Password is required when creating a user")
+    #                 )
+    #             user_vals = {
+    #                 "login": login,
+    #                 "password": password,
+    #                 "partner_id": partner.id,
+    #                 "groups_id": [(6, 0, [])],
+    #             }
+    #             user = self.env["res.users"].create(user_vals)
+    #             partner.user_id = user.id
 
-        return partners
+    #     return partners
 
-    def write(self, vals):
-        if "login" in vals:
-            new_login = vals["login"]
-            password = vals.pop("password", False)
-            for partner in self:
-                if partner.user_id:
-                    existing_user = self.env["res.users"].search(
-                        [("login", "=", new_login), ("id", "!=", partner.user_id.id)],
-                        limit=1,
-                    )
-                    if existing_user:
-                        raise exceptions.ValidationError(
-                            _("Login %s already exists in the system") % new_login
-                        )
-                    update_vals = {"login": new_login}
-                    if password:
-                        update_vals["password"] = password
-                    partner.user_id.write(update_vals)
-                else:
-                    if not password:
-                        raise exceptions.ValidationError(
-                            _("Password is required when creating a user")
-                        )
-                    user_vals = {
-                        "login": new_login,
-                        "password": password,
-                        "partner_id": partner.id,
-                        "groups_id": [(6, 0, [])],
-                    }
-                    user = self.env["res.users"].create(user_vals)
-                    partner.user_id = user.id
-        return super().write(vals)
+    # def write(self, vals):
+    #     if "login" in vals:
+    #         new_login = vals["login"]
+    #         password = vals.pop("password", False)
+    #         for partner in self:
+    #             if partner.user_id:
+    #                 existing_user = self.env["res.users"].search(
+    #                     [("login", "=", new_login), ("id", "!=", partner.user_id.id)],
+    #                     limit=1,
+    #                 )
+    #                 if existing_user:
+    #                     raise exceptions.ValidationError(
+    #                         _("Login %s already exists in the system") % new_login
+    #                     )
+    #                 update_vals = {"login": new_login}
+    #                 if password:
+    #                     update_vals["password"] = password
+    #                 partner.user_id.write(update_vals)
+    #             else:
+    #                 if not password:
+    #                     raise exceptions.ValidationError(
+    #                         _("Password is required when creating a user")
+    #                     )
+    #                 user_vals = {
+    #                     "login": new_login,
+    #                     "password": password,
+    #                     "partner_id": partner.id,
+    #                     "groups_id": [(6, 0, [])],
+    #                 }
+    #                 user = self.env["res.users"].create(user_vals)
+    #                 partner.user_id = user.id
+    #     return super().write(vals)
